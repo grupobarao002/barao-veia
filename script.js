d707c995e01699a5e752e5dfab5b22f3c097ec21
grupobarao002 <faduapassos264@gmail.com>
2024-09-27 11:15:33 -0300

    jogor

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-alert("Bem vindo");
+ let jogo
+
+function iniciajogo(){
+    const jogador1= document.getElementById('jogador1').value|| 'jogado1';
+    const jogador2= document.getElementById('jogador2').value||'jogado2';
+
+    jogo= new JogoDaVelha(jogador1,jogador2);
+    document.getElementById('entrada-nome').classList.add('oculto');
+    document.getElementById('jogo-container').classList.remove('oculto');
+    jogo.atualizarPlacar();
+
+    document.querySelectorAll('.celula').forEach(celular=>{
+        celular.addEventListener('click', function(){
+            jogo.jogada(this.getAttribute ('data-index'));
+        });
+    });
+}
